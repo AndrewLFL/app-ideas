b5d593987346610f043c2cfeabaf35eac42ba365
Andrew Luigi <andrewluigiflima@gmail.com>
2022-12-17 22:09:42 -0300

    New

---
+++ 1-Beginner/JSON2CSV_CSV2JSON/script.js
@@ -0,0 +1,55 @@
+//Textareas
+
+var json_01 = document.getElementById("json_01");
+var json_02 = document.getElementById("json_02");
+var csv_01 = document.getElementById("csv_01");
+var csv_02 = document.getElementById("csv_02");
+
+//Botoes
+
+var limpar_01 = document.getElementById("limpar_01");
+var limpar_02 = document.getElementById("limpar_02");
+var conv_json_csv = document.getElementById("conv_json_csv");
+var conv_csv_json = document.getElementById("conv_csv_json");
+var btnCopiar_01 = document.getElementById("btnCopiar_01");
+var btnCopiar_02 = document.getElementById("btnCopiar_02");
+
+function converter_json_csv ()
+{
+    console.log(json_01.value);
+}
+
+function converter_csv_json ()
+{
+    console.log(csv_02.value);
+}
+
+function copiar_01 ()
+{
+
+}
+
+function copiar_02()
+{
+
+}
+
+function limpar_01 ()
+{
+
+}
+
+function limpar_02 ()
+{
+
+}
+
+function validar_json()
+{
+
+}
+
+function validar_csv()
+{
+    
+}
